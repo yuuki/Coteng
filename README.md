1f0bf996774547e90cc82314beba8a45aca3779a
y_uuki <yuki.tsubo@gmail.com>
2013-12-28 15:17:53 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Coteng - It's new $module
+
+# SYNOPSIS
+
+    use Coteng;
+
+# DESCRIPTION
+
+Coteng is ...
+
+# LICENSE
+
+Copyright (C) y\_uuki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+y\_uuki <yuki.tsubo@gmail.com>
---
+++ lib/Coteng.pm
@@ -0,0 +1,39 @@
+package Coteng;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Coteng - It's new $module
+
+=head1 SYNOPSIS
+
+    use Coteng;
+
+=head1 DESCRIPTION
+
+Coteng is ...
+
+=head1 LICENSE
+
+Copyright (C) y_uuki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+y_uuki E<lt>yuki.tsubo@gmail.comE<gt>
+
+=cut
+
